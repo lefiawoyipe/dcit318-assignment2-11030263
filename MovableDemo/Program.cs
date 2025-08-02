619ea500ac0d5617482a57e9881427b67572d628
elikemleslie <alysonleslie658@gmail.com>
2025-08-02 22:26:14 +0000

    Interfaces

---
+++ MovableDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Interface
+interface IMovable
+{
+    void Move();
+}
+
+// Car implements IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Bicycle implements IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMovable[] movers = { new Car(), new Bicycle() };
+
+        foreach (var m in movers)
+        {
+            m.Move();
+        }
+    }
+}
+
