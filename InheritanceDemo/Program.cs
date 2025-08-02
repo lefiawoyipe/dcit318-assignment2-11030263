036774bfc327961555201159e59cbd0ce30b3163
elikemleslie <alysonleslie658@gmail.com>
2025-08-02 21:40:48 +0000

    Inheritance oop method

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// Base class
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal generic = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        Console.Write("An animal says: ");
+        generic.MakeSound(); 
+      
+
+        Console.Write("Dog says: ");
+        dog.MakeSound();    
+
+        Console.Write("Cat says: ");
+        cat.MakeSound();      
+    }
+}
